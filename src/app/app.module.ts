d78f6ddd1c273cd42d3db4c10f3eca14db705226
Jose <geafalconjoseantonio@gmail.com>
2023-11-01 01:40:09 +0100

    to do list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { TodoComponent } from './todo/todo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,89 @@
+import { Component } from '@angular/core';
+
+interface Task {
+  text: string;
+  completed: boolean;
+}
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+
+
+
+export class TodoComponent {
+  constructor() {
+    this.loadTasksFromLocal();
+  }
+  tasks: Task[] = [];
+  newTask: string = '';
+  filterOption = 'all';
+
+  loadTasksFromLocal() {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      this.tasks = JSON.parse(storedTasks);
+    }
+  }
+
+  addTask() {
+    if (this.newTask.trim()) {
+      const task: Task = {
+        text: this.newTask.trim(),
+        completed: false,
+      };
+      this.tasks.unshift(task); 
+      this.newTask = ''; // Limp
+      this.saveTasksToLocal();
+    }
+  }
+
+  removeTask(task: Task) {
+    const index = this.tasks.indexOf(task);
+    if (index > -1) {
+      this.tasks.splice(index, 1);
+      this.saveTasksToLocal();  // Actualiza las tareas en el localStorage
+    }
+  }
+  
+  sortTasks() {
+    this.tasks.sort((a, b) => {
+      if (a.completed && !b.completed) return 1;
+      if (!a.completed && b.completed) return -1;
+      return 0; 
+    });
+  }
+  
+  toggleCompletion(task: Task) {
+    task.completed = !task.completed;
+    this.sortTasks();
+    this.saveTasksToLocal();
+    if (task.completed) {
+      const index = this.tasks.indexOf(task);
+      if (index > -1) {
+        this.tasks.splice(index, 1);
+        this.tasks = [...this.tasks, task];
+      }else {
+        this.tasks.splice(index, 0);
+        this.tasks = [...this.tasks, task];
+      }
+    }
+  }
+  saveTasksToLocal() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+  get filteredTasks(): Task[] {
+    switch (this.filterOption) {
+      case 'all':
+        return this.tasks;
+      case 'pending':
+        return this.tasks.filter(task => !task.completed);
+      case 'completed':
+        return this.tasks.filter(task => task.completed);
+      default:
+        return this.tasks;
+    }
+  }
+}
